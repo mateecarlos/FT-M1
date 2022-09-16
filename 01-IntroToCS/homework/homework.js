4705785789635e297e3b670d82208437707b18af
mateecarlos <mateocarlos402@hotmail.com>
2022-09-16 18:10:20 -0300

    First Commit

---
+++ 01-IntroToCS/homework/homework.js
@@ -0,0 +1,26 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+  var sum = 0
+  var numInvertido = num.split('').reverse().join(''); // Invierto el numero
+
+  for (var i = 0; i < numInvertido.length; i++) {
+    sum = sum + numInvertido[i] * 2 ** i;
+  }
+
+  return sum;
+
+}
+
+function DecimalABinario(num) {
+  // tu codigo aca
+  
+  return num.toString(2)
+}
+
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
